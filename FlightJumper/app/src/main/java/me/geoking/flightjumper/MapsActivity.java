31c0c767c560192cf5d76c777f8e5fb4a80ac25b
Brandon Williams <brandinio@outlook.com>
2017-10-14 20:27:34 +0200

    Implement clickable map and add logic to the map marker

--- FlightJumper/app/src/main/java/me/geoking/flightjumper/MapsActivity.java
+++ FlightJumper/app/src/main/java/me/geoking/flightjumper/MapsActivity.java
@@ -1,7 +1,11 @@
 package me.geoking.flightjumper;
 
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,9 +14,14 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,10 +46,67 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        String location = "Big Ben, London";
+        final Geocoder gc = new Geocoder(this);
+        List<LatLng> ll = new ArrayList<LatLng>();
 
-        // Add a marker in Sydney and move the camera
-        LatLng bigBen = new LatLng(51.500606, -0.124582);
-        mMap.addMarker(new MarkerOptions().position(bigBen).title("Meet me at Big Ben"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(bigBen));
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            location = extras.getString("nameOfPlace");
+        }
+
+        if(Geocoder.isPresent()){
+            try {
+                List<Address> addresses= gc.getFromLocationName(location, 5); // get the found Address Objects
+
+                ll = new ArrayList<LatLng>(addresses.size()); // A list to save the coordinates if they are available
+                for(Address a : addresses){
+                    if(a.hasLatitude() && a.hasLongitude()){
+                        ll.add(new LatLng(a.getLatitude(), a.getLongitude()));
+                    }
+                    if(a.getFeatureName() != null) {
+                        location = a.getFeatureName();
+                    }
+                }
+            } catch (IOException e) {
+                // handle the exception
+            }
+        }
+        LatLng coord = ll.get(0);
+        mMap.addMarker(new MarkerOptions().position(coord).title(location).flat(true));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(coord));
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(5));
+
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+
+            @Override
+            public void onMapClick(LatLng arg0) {
+
+                List<String> countries = new ArrayList<String>();
+                try {
+                    List<Address> addresses = gc.getFromLocation(arg0.latitude, arg0.longitude, 5);
+                    countries = new ArrayList<String>(); // A list to save the coordinates if they are available
+                    for(Address a : addresses){
+                        if(a.getCountryName() != null){
+                            countries.add(a.getCountryName());
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Context context = getApplicationContext();
+                CharSequence text = countries.get(0);
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+
+            }
+        });
+    }
+
+    private void onMapClick(LatLng arg0) {
+
+
     }
 }
--- FlightJumper/app/src/main/java/me/geoking/flightjumper/StartActivity.java
+++ FlightJumper/app/src/main/java/me/geoking/flightjumper/StartActivity.java
@@ -1,13 +1,34 @@
 package me.geoking.flightjumper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.text.TextWatcher;
+import android.text.Editable;
 
-public class StartActivity extends AppCompatActivity {
+public class StartActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText yourEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        yourEditText = (EditText) findViewById(R.id.originLocation);
+
+        Button enterButton = (Button) findViewById(R.id.button2);
+        enterButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i = new Intent(this, MapsActivity.class);
+        i.putExtra("nameOfPlace", yourEditText.getText().toString());
+        startActivity(i);
     }
 }
